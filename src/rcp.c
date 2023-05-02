85f8218d26f0343cce47a8cf8241b3bc9f6cc50b
신동현 <sindonghyun002@gmail.com>
2023-05-02 06:52:10 +0000

    src

--- src/rcp.c
+++ src/rcp.c
@@ -1,17 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 int main() {
-	int com, user, win;
+	int com, win = 0;
+	char user[10];
+	
 	srand(time(NULL));
 	com = rand() % 3 + 1;
 	
-	printf("주먹:1, 가위:2, 보:3 : ");
-	scanf("%d", &user);
+	printf("가위바위보!\n");
+	scanf("%s", user);
 	
 	if(com == 1) {
-		printf("")
-	}
+		printf("com : 바위\n");
+		if (strcmp(user, "바위") == 0) {
+			win = 3;
+		} else if(strcmp(user, "가위") == 0) {
+			win = 2;
+		} else {
+			win = 1;
+		}
+	} else if(com == 2) {
+		printf("com : 가위\n");
+		if (strcmp(user, "바위") == 0) {
+			win = 1;
+		} else if(strcmp(user, "가위") == 0) {
+			win = 3;
+		} else {
+			win = 2;
+		}
+	} else {
+		printf("com : 보\n");
+		if (strcmp(user, "바위") == 0) {
+			win = 2;
+		} else if(strcmp(user, "가위") == 0) {
+			win = 1;
+		} else {
+			win = 3;
+		}
+	};
 	
+	if(win == 1) {
+		printf("이겼다!\n");
+	}else if(win == 2) {
+		printf("졌다...\n");
+	}else {
+		printf("비겼다\n");
+	};
+	
+	return 0;
 }
