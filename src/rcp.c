a8d36c65f71b80ff80d0d8710544328559d9af24
신동현 <sindonghyun002@gmail.com>
2023-05-02 05:49:32 +0000

    src

---
+++ src/rcp.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+	int com, user, win;
+	srand(time(NULL));
+	com = rand() % 3 + 1;
+	
+	printf("주먹:1, 가위:2, 보:3 : ");
+	scanf("%d", &user);
+	
+	if(com == 1) {
+		printf("")
+	}
+	
+}
